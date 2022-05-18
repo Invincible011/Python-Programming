76ebcbd59dc5d387c1779f3987cc97b85d4b8831
AbdulMumin Ohinoyi <87006597+Invincible011@users.noreply.github.com>
2022-05-18 21:50:32 +0100

    Update day 2 - assignment.py

--- day 2 - assignment.py
+++ day 2 - assignment.py
@@ -6,10 +6,10 @@ try:
     rate = float(rate)
     if hours >= 40:
         pay = (hours * rate) + 25
-        print('Payment Overtime=', pay)
+        print('Payment Overtime =', pay)
     else:
         pay = hours * rate
-        print('Payment without overtime=', pay)
+        print('Payment without overtime =', pay)
     print()
 except:
     print('Error, please enter a number')
@@ -20,11 +20,11 @@ try:
     score = float(score)
     if score  >= 0.9 and score <= 0.99:
         print('A')
-    elif score >= 0.8 and score <=0.89:
+    elif score >= 0.8 and score <= 0.89:
         print('B')
-    elif score >= 0.7 and score <=0.79:
+    elif score >= 0.7 and score <= 0.79:
         print('C')
-    elif score >= 0.6 and score <=0.69:
+    elif score >= 0.6 and score <= 0.69:
         print('D')
     elif score < 0.6:
         print('F')
